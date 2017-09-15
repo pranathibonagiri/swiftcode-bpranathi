1d99e2db40908a1cb47cc0b92e116639cb81f6cf
pranathi <pranathibonagiri96@gmail.com>
2017-09-15 06:22:16 +0000

    feedresponsefinished

---
+++ app/data/Message.java
@@ -0,0 +1,11 @@
+package data;
+
+
+public class Message {
+    public String test;
+    public FeedResponse feedRessponse;
+    public Sender sender;
+    public enum Sender{
+        BOT, USER
+    }
+}
--- app/services/AgentService.java
+++ app/services/AgentService.java
@@ -6,6 +6,7 @@ import play.libs.ws.WS;
 import play.libs.ws.WSRequest;
 import play.libs.ws.WSResponse;
 
+import java.util.UUID;
 import java.util.concurrent.CompletionStage;
 
 public class AgentService {
@@ -17,7 +18,7 @@ public class AgentService {
                     .setQueryParameter("v", "20150910")
                     .setQueryParameter("query",text)
                     .setQueryParameter("lang", "en")
-                    .setQueryParameter("sessionId", sessionId.toString())
+                    .setQueryParameter("sessionId", UUID.randomUUID().toString())
                     .setQueryParameter("timezone", "2017-08-09T03:25:23+0530")
                     .setHeader("Authorization", "Bearer e8a14f93846741d09b487d2825158f5b")
                     .get();
