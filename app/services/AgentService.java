55551d2e5244da114536c959bcfadabdcd8c1c6b
pranathi <pranathibonagiri96@gmail.com>
2017-09-16 10:30:43 +0000

    showing results

--- app/services/AgentService.java
+++ app/services/AgentService.java
@@ -10,9 +10,9 @@ import java.util.UUID;
 import java.util.concurrent.CompletionStage;
 
 public class AgentService {
-    public AgentResponse getAgentResponse(String text){
+    public AgentResponse getAgentResponse(String text) {
         AgentResponse agentResponse = new AgentResponse();
-        try{
+        try {
             WSRequest queryRequest = WS.url("https://api.api.ai/api/query");
             CompletionStage<WSResponse> responsePromise = queryRequest
                     .setQueryParameter("v", "20150910")
@@ -20,14 +20,12 @@ public class AgentService {
                     .setQueryParameter("lang", "en")
                     .setQueryParameter("sessionId", UUID.randomUUID().toString())
                     .setQueryParameter("timezone", "2017-08-09T03:25:23+0530")
-                    .setHeader("Authorization", "Bearer e8a14f93846741d09b487d2825158f5b")
+                    .setHeader("Authorization", "Bearer 946df4ead6524dbcaeb5c6c2409462b6")
                     .get();
             JsonNode response = responsePromise.thenApply(WSResponse::asJson).toCompletableFuture().get();
-            agentResponse.keyword = response.get("result").get("parameters").get("q").asText();
-
-        }catch(Exception e){
-            e.printStackTrace();
-
+            agentResponse.keyword = response.get("result").get("parameters").get("keyword").asText();
+        } catch(Exception ex) {
+            ex.printStackTrace();
         }
         return agentResponse;
     }
